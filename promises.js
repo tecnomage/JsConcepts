5ade7ed09d8273193b2097d5074abfd3bcf59648
tecnomage <tecnocratay@gmail.com>
2019-05-02 17:07:36 -0300

    transformacao em arrow function

--- promises.js
+++ promises.js
@@ -1,10 +1,12 @@
 const p = new Promise((resolve, reject) => {
   if (Math.random() > 0.5) resolve('yay')
-  reject('no')
+  reject('nOOoOoOoOoOo')
 })
 
 p
-.then(function acao1 (res) { console.log(`${res} da ação 1`); return res; })
-.then(function acao2 (res) { console.log(`${res} da ação 2`); return res; })
-.then(function acao3 (res) { console.log(`${res} da ação 3`); return res; })
-.catch(function erro (rej) { console.error(rej) })
+.then((res) => { console.log(`${res} da ação 1`); return res; })
+//diversos catchs em uma promisse
+//.catch(function erro1 (err) { console.error('Primeiro catch'); return 'Error'; })
+.then((res) => { console.log(`${res} da ação 2`); return res; })
+.then((res) => { console.log(`${res} da ação 3`); return res; })
+.catch((rerr) => { console.error(rerr)})
