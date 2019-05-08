4ea8868f3206abbe1f49842fbf79203d8fdc0f2c
tecnomage <tecnocratay@gmail.com>
2019-05-08 10:56:02 -0300

    problema do catch no promisses 2 resolvido

--- promises2.js
+++ promises2.js
@@ -1,18 +1,33 @@
 const p = new Promise((resolve, reject) => {
-  if (Math.random() > 0.5) resolve('yay')
-        reject('NOoooOoo')
+  if (Math.random() > 0.5) resolve("yay");
+  reject("NOoooOoo");
+});
+
+p.catch(err => {
+  console.log("Primeiro catch");
+  return "Error";
 })
+// .catch(e => {
+//   console.log("2o catch");
+// });
 
-
-
-// Veja que estamos definindo o catch diretamente em p
-//p.catch(function erro1 (err) { console.error('Primeiro catch'); return 'Error'; })
-p.catch(function erro1(err) { console.('Primeiro catch'); return 'Error'; })
-p.catch(err => console.log(err))
-//p.catch(function erro (rej) { console.error(rej) })
+p.catch(e => {
+  console.log("3o catch");
+});
 
 // Estamos definindo as chamadas dos then's também diretamente em p
-p
-.then(function acao1 (res) { console.log(`${res} da ação 1`); return res; })
-.then(function acao2 (res) { console.log(`${res} da ação 2`); return res; })
-.then(function acao3 (res) { console.log(`${res} da ação 3`); return res; })
+p.then(function acao1(res) {
+  console.log(`${res} da ação 1`);
+  return res;
+})
+  .then(function acao2(res) {
+    console.log(`${res} da ação 2`);
+    return res;
+  })
+  .then(function acao3(res) {
+    console.log(`${res} da ação 3`);
+    return res;
+  })
+  .catch(saida => {
+    console.log("ultimo");
+  });
